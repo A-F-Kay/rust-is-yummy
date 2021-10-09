f5b038d64890ef907e0c5264f7925cbf725d74fb
A. F. Kay <afkay.dev@pm.me>
2021-10-10 01:28:19 +0300

    Add basic types usage (basically printing)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+#[allow(dead_code)] // FIXME: remove?
+mod types;
+
 fn main() {
+    let (id, username): types::SimpleUser = (9000, String::from("afkay"));
+    let afkay = types::User {
+        id,
+        username,
+        gender: types::Gender::Trans(types::Sex::Female),
+    };
+
     println!("Hello, world!");
-    println!("Another commit added");
-    println!("And another one");
+    println!("{:?}", afkay);
 }
