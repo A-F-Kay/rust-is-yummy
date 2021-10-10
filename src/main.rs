5d44ab72cfffcdc788bb24f7cab2db34c50eb5fc
A. F. Kay <afkay.dev@pm.me>
2021-10-10 04:39:33 +0300

    Add User constructor, Add age calculation, make User fields private

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,17 @@ mod guess_game;
 mod user_types;
 
 fn main() {
-    let (id, username): user_types::SimpleUser = (9000, String::from("afkay"));
-    let afkay = user_types::User {
-        id,
-        username,
-        gender: user_types::Gender::Trans(user_types::Sex::Female),
-        born_at: Date::from(Utc.ymd(1997, 3, 3)),
-        name: (String::from("Lara"), None),
-        country: Some(user_types::Country::UA),
-    };
+    let afkay_basic: user_types::SimpleUser = (9000, String::from("afkay"));
+    let afkay = user_types::User::new(
+        afkay_basic,
+        user_types::Gender::Trans(user_types::Sex::Female),
+        (String::from("Lara"), None),
+        Date::from(Utc.ymd(1997, 3, 3)),
+        Some(user_types::Country::UA),
+    );
 
     let game = GuessGame {
-        win_number: 24,
+        win_number: afkay.get_age(),
         guess_range: (1, 100),
         hello_message: String::from("Hi. Please guess my age :)"),
     };
@@ -36,7 +35,7 @@ fn main() {
             Ok(()) => {
                 println!("Nice one! You guessed it right! :)");
                 break;
-            },
+            }
             Err(GuessError::OutOfRange) => {
                 println!("Out of range. Valid range: {}", game.range_str());
                 continue;
@@ -55,7 +54,4 @@ fn main() {
             }
         };
     }
-
-    println!("Hello, world!");
-    println!("{:#?}", afkay);
 }
--- src/user_types.rs
+++ src/user_types.rs
@@ -1,5 +1,6 @@
 use chrono::prelude::Date;
-use chrono::Utc;
+use chrono::{Datelike, Duration, Local, Utc};
+use std::ops::Sub;
 
 // id, username
 pub type SimpleUser = (i32, String);
@@ -30,10 +31,50 @@ pub enum Country {
 
 #[derive(Debug)]
 pub struct User {
-    pub id: i32,
-    pub username: String,
-    pub gender: Gender,
-    pub name: UserName,
-    pub born_at: Date<Utc>,
-    pub country: Option<Country>,
+    id: i32,
+    username: String,
+    gender: Gender,
+    name: UserName,
+    born_at: Date<Utc>,
+    country: Option<Country>,
+}
+
+impl User {
+    pub fn new(
+        basic_info: SimpleUser,
+        gender: Gender,
+        name: UserName,
+        born_at: Date<Utc>,
+        country: Option<Country>,
+    ) -> Self {
+        let (id, username) = basic_info;
+
+        User {
+            id,
+            username,
+            gender,
+            name,
+            born_at,
+            country,
+        }
+    }
+
+    pub fn get_age(&self) -> i32 {
+        let today = Utc::now().date();
+        let (month, day) = (today.month(), today.day());
+
+        let (born_month, born_day) = (self.born_at.month(), self.born_at.day());
+
+        let passed_birthday_this_year = if born_month < month {
+            true
+        } else if born_month == month {
+            born_day >= day
+        } else {
+            false
+        };
+
+        let age_modifier = if passed_birthday_this_year { 0 } else { -1 };
+
+        (today.year() - self.born_at.year()) + age_modifier
+    }
 }
